be5c70b5809ef277769fc28809091e90da795083
ashurohom <ashitosh.rohom@gmail.com>
2025-09-01 21:24:43 +0530

    add bg image

--- gurudatt/src/App.jsx
+++ gurudatt/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './css/app.css'
+import bg5 from './assets/bg5.jpg'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Gurudatt Electricals  
-      </h1>
+    <div className="h-screen w-full relative">
+      <div
+        className="absolute inset-0 bg-cover bg-center filter blur-[2px]" 
+        style={{ backgroundImage: `url(${bg5})` }}
+      ></div>
+
+      <div className="relative z-10">
+        <h1 className="text-white text-4xl font-bold">Gurudatt Electrical</h1>
+      </div>
     </div>
   )
 }
