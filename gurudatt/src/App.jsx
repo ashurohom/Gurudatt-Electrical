5f5954bf6078cc19983aa79e9ea5bec01d307629
ashurohom <ashitosh.rohom@gmail.com>
2025-09-01 21:54:42 +0530

    add logo

--- gurudatt/src/App.jsx
+++ gurudatt/src/App.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import './css/app.css'
-import bg5 from './assets/bg5.jpg'
+import bg2 from './assets/bg2.jpg'
+import GE2 from './assets/GE2.png'
 
 function App() {
   return (
     <div className="h-screen w-full relative">
       <div
-        className="absolute inset-0 bg-cover bg-center filter blur-[2px]" 
-        style={{ backgroundImage: `url(${bg5})` }}
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: `url(${bg2})` }}
       ></div>
 
-      <div className="relative z-10">
-        <h1 className="text-white text-4xl font-bold">Gurudatt Electrical</h1>
+      <div className="absolute inset-0 bg-black opacity-80"></div>
+
+        <div className="relative z-10 h-full flex flex-col">
+        <div className="p-4">
+          <img
+            src={GE2}
+            alt="Gurudatt Electrical Logo"
+            className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-full border-4 border-white shadow-lg object-cover"
+          />
+        </div>
+        </div>
+
+
+      <div className="relative z-10 flex items-center justify-center h-full">
+        <h1 className="text-white text-4xl font-bold">
+          Gurudatt Electrical
+        </h1>
       </div>
     </div>
   )
