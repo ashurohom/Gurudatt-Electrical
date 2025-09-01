06462ab5c1ce0857bc1d9130ff8623cc4719677b
ashurohom <ashitosh.rohom@gmail.com>
2025-09-01 21:59:08 +0530

    add logo

--- gurudatt/src/App.jsx
+++ gurudatt/src/App.jsx
@@ -6,6 +6,7 @@ import GE2 from './assets/GE2.png'
 function App() {
   return (
     <div className="h-screen w-full relative">
+      {/* Background image */}
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{ backgroundImage: `url(${bg2})` }}
@@ -13,19 +14,21 @@ function App() {
 
       <div className="absolute inset-0 bg-black opacity-80"></div>
 
-        <div className="relative z-10 h-full flex flex-col">
+      {/* Foreground content */}
+      <div className="relative z-10 h-full flex flex-col">
         <div className="p-4">
-          <img
-            src={GE2}
-            alt="Gurudatt Electrical Logo"
-            className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-full border-4 border-white shadow-lg object-cover"
-          />
+          <div className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 rounded-full border-4 border-white flex items-center justify-center shadow-lg">
+            <img
+              src={GE2}
+              alt="Gurudatt Electrical Logo"
+              className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-full object-cover"
+            />
+          </div>
         </div>
-        </div>
-
+      </div>
 
       <div className="relative z-10 flex items-center justify-center h-full">
-        <h1 className="text-white text-4xl font-bold">
+        <h1 className="text-white text-4xl md:text-6xl font-bold text-center">
           Gurudatt Electrical
         </h1>
       </div>
