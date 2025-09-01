c5ade863a300e23d3cac35b3233b36f417360863
ashurohom <ashitosh.rohom@gmail.com>
2025-09-01 23:07:09 +0530

    update  navbar

--- gurudatt/src/components/Header.jsx
+++ gurudatt/src/components/Header.jsx
@@ -6,8 +6,9 @@ function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="w-full flex items-center justify-between p-4 bg-transparent fixed top-0 left-0 z-50">
-      <div className="flex items-center">
+    <header className="w-full flex items-center p-4 bg-transparent fixed top-0 left-0 z-50">
+      {/* Logo on the left */}
+      <div className="flex items-center z-10">
         <div className="w-12 h-12 sm:w-16 sm:h-16 rounded-full border-4 border-white flex items-center justify-center shadow-lg">
           <img
             src={GE2}
@@ -17,9 +18,9 @@ function Header() {
         </div>
       </div>
 
-      {/* Desktop Navbar */}
-      <nav className="hidden sm:block">
-        <ul className="flex space-x-6 text-white text-lg sm:text-xl">
+      {/* Desktop Navbar centered */}
+      <nav className="hidden sm:block absolute left-1/2 transform -translate-x-1/2 z-0">
+        <ul className="flex space-x-8 text-white text-xl sm:text-1xl font-medium font-poppins">
           <li>
             <Link
               to="/"
@@ -63,13 +64,12 @@ function Header() {
         </ul>
       </nav>
 
-      {/* Mobile Hamburger Button */}
-      <div className="sm:hidden">
+      {/* Mobile Hamburger Button on right */}
+      <div className="sm:hidden ml-auto z-10">
         <button
           onClick={() => setIsOpen(!isOpen)}
           className="focus:outline-none text-white"
         >
-          {/* Hamburger icon */}
           <svg
             className="w-8 h-8"
             fill="none"
@@ -83,8 +83,8 @@ function Header() {
               strokeWidth="2"
               d={
                 isOpen
-                  ? "M6 18L18 6M6 6l12 12" // X icon when open
-                  : "M4 6h16M4 12h16M4 18h16" // Hamburger when closed
+                  ? "M6 18L18 6M6 6l12 12" // X icon
+                  : "M4 6h16M4 12h16M4 18h16" // Hamburger
               }
             ></path>
           </svg>
@@ -94,7 +94,7 @@ function Header() {
       {/* Mobile Navbar */}
       {isOpen && (
         <nav className="absolute top-full left-0 w-full bg-black bg-opacity-90 sm:hidden">
-          <ul className="flex flex-col items-center space-y-4 py-4 text-white text-lg">
+            <ul className="flex flex-col items-center space-y-4 py-4 text-white text-xl font-medium font-poppins">
             <li>
               <Link
                 to="/"
@@ -111,6 +111,15 @@ function Header() {
                 onClick={() => setIsOpen(false)}
               >
                 About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/product"
+                className="hover:text-yellow-400 hover:underline transition-colors"
+                onClick={() => setIsOpen(false)}
+              >
+                Product
               </Link>
             </li>
             <li>
